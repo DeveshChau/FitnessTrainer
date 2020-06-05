68d0ef240d94ec2ed57fb262286065ed2768f435
DeveshChau <deveshachaudhari@gmail.com>
2020-06-05 19:09:02 +0530

    environment prod with firebaseConfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBn5I86GnYB5EHR2z6xZTPwWaJq23XguvU',
+    authDomain: 'fintness-trainer.firebaseapp.com',
+    databaseURL: 'https://fintness-trainer.firebaseio.com',
+    projectId: 'fintness-trainer',
+    storageBucket: 'fintness-trainer.appspot.com',
+    messagingSenderId: '606108247392',
+    appId: '1:606108247392:web:b2056d3b8e6a17c0ac2a05'
+  }
 };
